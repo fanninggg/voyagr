a614e50ef36c3c993288ef990736f1decd5f2cbd
Angele Lenglemetz <angele.lenglemetz@gmail.com>
2018-03-05 16:11:03 +0000

    created the chatbot youhou

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,53 @@
+class WebhooksController < ApplicationController
+  skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
+  def messenger
+    if params["hub.verify_token"] == ENV['MESSENGER_VERIFICATION_TOKEN']
+      render plain: params["hub.challenge"]
+    else
+      render plain: "error"
+    end
+  end
+
+  def receive_message
+    puts "asdf"
+    therequest = request.body.read
+    data = JSON.parse(therequest)
+    entries = data["entry"]
+    sender = nil
+    text = nil
+    my_reply = nil
+    entries.each do |entry|
+      entry["messaging"].each do |messaging|
+        if messaging["read"]
+          # do nothing
+        elsif messaging["delivery"]
+          #do nothing
+        elsif messaging["postback"]
+          # Error check
+          if messaging["postback"]["payload"] == "We are Voyagr!"
+            puts "hey world"
+            # sender = messaging["sender"]["id"]
+            # my_reply = get_started_reply(sender)
+          end
+        elsif messaging["message"]
+          # sender = messaging["sender"]["id"]
+          # text = messaging["message"]["text"]
+          if text == "help"
+            puts "holla"
+          else
+            puts "salut"
+          end
+          HTTP.post(url, json: my_reply)
+        end
+      end
+      puts my_reply
+      render plain: my_reply
+    end
+  end
+
+  def url
+    "https://graph.facebook.com/v2.6/me/messages?access_token=#{ENV['ACCESS_TOKEN']}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'webhooks', to: 'webhooks#messenger'
+  post 'webhooks', to: 'webhooks#receive_message'
+
 end
