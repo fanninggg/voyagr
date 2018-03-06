074b9f898201904ac311233e7888808d8ec3c494
Angele Lenglemetz <angele.lenglemetz@gmail.com>
2018-03-06 18:28:15 +0000

    half of database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,33 +9,732 @@
 
 cities_attributes = [
   {
-    name: 'Venice, Italy'
+    name: 'Venice, Italy',
     budget:         'high',
     location:      'Europe',
     type_of_evening:  'Restaurant',
     type_of_city:        'Cultural'
   },
   {
-    name: 'Seville, Spain'
+    name: 'Seville, Spain',
     budget:         'low',
     location:      'Europe',
     type_of_evening:  'Bar',
     type_of_city:   'Cultural'
   },
     {
-    name: 'Santorini, Greece'
+    name: 'Santorini, Greece',
     budget:         'Medium',
     location:      'Europe',
     type_of_evening:  'Restaurant',
     type_of_city:   'Relaxing'
   },
     {
-    name: ' Rio De Janeiro, Brazil'
+    name: ' Rio De Janeiro, Brazil',
     budget:         'Medium',
     location:      'Outside',
     type_of_evening:  'Bar',
     type_of_city:   'Adventurous'
   },
+    {
+    name: 'Gdansk, Poland',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+     {
+    name: 'Canary Islands, Spain',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },   {
+    name: 'Dublin, Ireland',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },   {
+    name: 'Budapest, Hungary',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },   {
+    name: 'Berlin, Germany',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+  {
+    name: ' Prague, Czech Republic',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+  {
+    name: 'Dubrovnik, Croatia',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Krakow, Poland',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+    {
+    name: 'Madeira, Portugal',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+  {
+    name: 'Stari Grad, Croatia',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+  {
+    name: 'Malta Island, Malta',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Glasgow, UK',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Sarajevo, Bosnia and Herzegovina',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Prague, Czech Republic',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Bruges, Belgium',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Brighton, UK',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+ {
+    name: 'Vienna, Austria',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+ {
+    name: 'Madeira, Portugal',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Canary Islands, Spain',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Kotor, Montenegro',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+     {
+    name: 'Sofia, Bulgaria',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+     {
+    name: 'Minsk, Belarus',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+     {
+    name: 'Madrid, Spain',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Sozopol, Bulgaria',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+  {
+    name: 'Granada, Spain',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+  {
+    name: 'Paphos, Cyprus',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Sozopol, Bulgaria',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Granada, Spain',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Paphos, Cyprus',
+    budget:         'low',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+     {
+    name: ' Cartagena, Colombia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: 'Bangkok, Thailand',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: ' Antigua Guatemala, Guatemala',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+ {
+    name: 'Cusco, Peru',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Valparaiso, Chile',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Kuala Lumpa, Malaysia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Boracay, Philippines',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Recife, Brazil',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Bogota, Columbia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Bogota, Columbia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+    {
+    name: 'Delhi, India',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+  {
+    name: 'Oaxaca, Mexico',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+  {
+    name: 'Addis Ababa, Ethiopia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Mumbai, India',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'Angkor Wat, Cambodia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'Casablanca, Morocco',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+ {
+    name: 'Tunis, Tunisia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+ {
+    name: 'Cancun, Mexico',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+ {
+    name: 'Jaipur, India',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Lahore, Pakistan',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  }, {
+    name: 'Caracas, Venezuela',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+  {
+    name: 'Tehran, Iran',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+  {
+    name: 'Siem Reap, Cambodia',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+  {
+    name: 'Colombo, Sri Lanka',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Hội An, Vietnam',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+  {
+    name: 'Montevideo, Uruguay',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+  {
+    name: 'Vientiane, Laos',
+    budget:         'low',
+    location:      'Worldwide',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Dublin, Ireland',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: 'Ibiza, Spain',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: 'Bonifacio, France',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: 'Vienna, Austria',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+      {
+    name: 'Cambridge, England',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+      {
+    name: 'Barcelona, Spain',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'Bilbao, Spain',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+  {
+    name: 'Lyon, France',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+  {
+    name: 'Bratislava, Slovakia',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+    {
+    name: 'Lisbon, Portugal',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Mont Saint Michel, France',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Krk, Croatia',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: 'Athens, Greece',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+  {
+    name: 'Milan, Italy',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+  {
+    name: 'Bucharest, Romania',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'Lyon, France',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+     {
+    name: 'Oia, Greece',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Porto, Portugal',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: 'La Grave, France',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+    {
+    name: 'Isle of Skye, UK',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Lisbon, Portugal',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Munich, Germany',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Burgos, Spain',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Cork, Ireland',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+     {
+    name: 'Split, Croatia',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+     {
+    name: 'Helsinki, Finland',
+    budget:         'Medium',
+    location:      'Europe',
+    type_of_evening:  'Meet the locals',
+    type_of_city:        'Relaxing'
+  },
+   {
+    name: 'Rio De Janeiro, Brazil',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+     {
+    name: 'Chicago, Illinois',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+     {
+    name: 'Moorea, Tahiti',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Adventurous'
+  },
+     {
+    name: 'Nairobi, Kenya',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'St. Petersburg, Russia',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'Sacred Valley, Peru',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Culturous'
+  },
+     {
+    name: 'Montreal, Canada',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+       {
+    name: 'Montego Bay, Jamaica',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+
+     {
+    name: 'Suva, Fiji',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Bar',
+    type_of_city:        'Relaxing'
+  },
+  {
+    name: 'Beirut, Lebanon',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: ' Burlington, US',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: 'Jakarta, Indonesia',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Adventurous'
+  },
+   {
+    name: ' Istanbul, Turkey',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'Buenos Aires, Argentina',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+    {
+    name: 'Beijing, China',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Culturous'
+  },
+   {
+    name: 'Marrakech, Moroccoa',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+    {
+    name: 'Hamilton, Burmuda',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+    {
+    name: 'Papeete, Bora Bora',
+    budget:         'Medium',
+    location:      'Worldwide',
+    type_of_evening:  'Restaurant',
+    type_of_city:        'Relaxing'
+  },
+
+
 ]
 
 City.create!(cities_attributes)
