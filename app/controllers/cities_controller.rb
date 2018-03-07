b3d512ff898230275bd676c10fc7e23f9840520e
Hen Brand <hencbrand@gmail.com>
2018-03-07 18:00:28 +0000

    added map markers kind of -needs fixing

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,6 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
 
-
     @cities = City.where.not(latitude: nil, longitude: nil)
 
     @markers = @cities.map do |flat|
@@ -13,9 +12,20 @@ class CitiesController < ApplicationController
       }
     end
 
+
+
   end
 
   def show
     @city = City.find(params[:id])
+    @cities = City.where.not(latitude: nil, longitude: nil)
+
+    @markers = @cities.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
 end
