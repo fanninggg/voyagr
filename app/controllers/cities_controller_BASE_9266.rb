46d715400cd359f9f8a2676b2137060bf4b7ab3d
Angele Lenglemetz <angele.lenglemetz@gmail.com>
2018-03-09 11:25:24 +0000

    play api

--- app/controllers/cities_controller_BASE_9266.rb
+++ app/controllers/cities_controller_BASE_9266.rb
@@ -25,7 +25,6 @@ class CitiesController < ApplicationController
         lng: @city.longitude,
         icon: "http://res.cloudinary.com/dm2e6swvo/image/upload/c_scale,w_50/v1520525872/voyagr/black-pin.png"#,
         # infoWindow: { content: render_to_string(partial: "/cities/map_box", locals: { city: city }) }
-<<<<<<<<< Temporary merge branch 1
       }]
 
     params = {
@@ -37,31 +36,6 @@ class CitiesController < ApplicationController
 
     response =  RestClient.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {params: params})
     @hotel = JSON.parse(response)
-
-=========
-      }
-    ]
-    # check_in_date = 2.weeks.from_now
-    # check_out_date = 4.weeks.from_now
-    # params = {
-    #   apikey: "LVZKpIaTnRcGpiHZrVqA6tS4xJNyZUFC",
-    #   latitude: @city.latitude,
-    #   longitude: @city.longitude,
-    #   radius: 10,
-    #   check_in: check_in_date,
-    #   check_out: check_out_date
-    # }
-
-    # RestClient.get('https://api.sandbox.amadeus.com/v1.2/hotels/search-circle', params, headers: {apikey: "LVZKpIaTnRcGpiHZrVqA6tS4xJNyZUFC"})
-    # url: 'https://api.sandbox.amadeus.com/v1.2/hotels/search-circle',
-    # {apikey: "LVZKpIaTnRcGpiHZrVqA6tS4xJNyZUFC",
-    # latitude: @city.latitude,
-    # longitude: @city.longitude,
-    # radius: 10,
-    # check_in: check_in_date,
-    # check_out: check_out_date}
-  end
->>>>>>>>> Temporary merge branch 2
 
 
   end
