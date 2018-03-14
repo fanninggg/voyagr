d9e9734c0415acd67265e0f752f4f1bdf23df4e8
Celia Collins <celia.collins@hotmail.com>
2018-03-14 18:39:46 +0000

    fixed stuff

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,18 +5,8 @@ require 'open-uri'
 
 class CitiesController < ApplicationController
   def index
-
-    if @cities = City.where.not(latitude: nil, longitude: nil)
-       @markers = @cities.map do |city|
-        {
-          lat: city.latitude,
-          lng: city.longitude,
-        }
-      end
-    else
-      @cities = City.all
-    end
-    @city = City.all.sample(3)
+    @cities = City.all.sample(3)
+    @city = @cities.sample
   end
 
   def show
