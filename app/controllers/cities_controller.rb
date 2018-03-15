a35e417dec0aae4bb8e04d46cd8a0d1924f71b52
Hen Brand <hencbrand@gmail.com>
2018-03-15 15:38:03 +0000

    ready for heroku

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -72,26 +72,6 @@ class CitiesController < ApplicationController
         counter += 1
         start_date = start_date.next_week
       end
-
-
-      # if flight.length < 1
-      #   url = "https://api.skypicker.com/flights?flyFrom=#{@city_from}&to=#{@city_to}&dateFrom=#{next_friday.strftime("%d/%m/%Y")}&dateTo=#{next_saturday.strftime("%d/%m/%Y")}&curr=GBP&limit=1&directFlights=1"
-      #   response_flight = RestClient.get(url)
-      #   flight = JSON.parse(response_flight)
-      #   if flight.length == 0
-      #     puts "You May Have To Find Another Way To Get Here!"
-      #   end
-      # end
-      #     airline = flight["data"].first["airlines"].first
-      #     @hash_out[:airline] << airline
-      #     @hash_out[:price] << flight["data"].first["conversion"]["GBP"]
-      #     @hash_out[:logo] << "https://images.kiwi.com/airlines/64/#{airline}.png"
-      #     @hash_out[:departure] << flight["data"].first["routes"].first.first
-      #     @hash_out[:arrival] << flight["data"].first["routes"].first.second
-      #     @hash_out[:dtime] << flight["data"].first["dTimeUTC"]
-      #     @hash_out[:atime] << flight["data"].first["aTimeUTC"]
-      #     counter += 1
-      #     start_date = start_date.next_week
     end
   end
 
@@ -120,22 +100,6 @@ class CitiesController < ApplicationController
       url = "https://api.skypicker.com/flights?flyFrom=#{@city_from}&to=#{@city_to}&dateFrom=#{next_sunday.strftime("%d/%m/%Y")}&dateTo=#{next_monday.strftime("%d/%m/%Y")}&curr=GBP&limit=1"
       response_flight = RestClient.get(url)
       flight = JSON.parse(response_flight)
-      # puts 'xxxx'
-      # puts "flight length"
-      # puts flight.length
-      # puts 'xxxx'
-      # puts "flight data"
-      # puts flight["data"]
-      # puts "xxxx"
-      # puts "flight data .first"
-      # puts flight["data"].first
-      # puts "xxxx"
-      # puts "flight data .first airlines"
-      # puts flight["data"].first["airlines"]
-      # puts "xxxx"
-      # puts "flight data .first airlines .first"
-      #  puts flight["data"].first["airlines"].first
-
 
       if flight["data"].empty?
         url = "https://api.skypicker.com/flights?flyFrom=#{@city_from}&to=#{@city_to}&dateFrom=#{next_sunday.strftime("%d/%m/%Y")}&dateTo=#{next_monday.strftime("%d/%m/%Y")}&curr=GBP&limit=1&directFlights=1"
